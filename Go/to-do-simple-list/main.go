d03a33501ef97c0efcc8f2b7d9d878029bc5a970
RenatoFaraco <renato.faraco.f@gmail.com>
2025-05-06 22:12:34 -0300

    feat: add `remove`, `clear` and `help` commands

--- Go/to-do-simple-list/main.go
+++ Go/to-do-simple-list/main.go
@@ -11,8 +11,8 @@ func main() {
 	tasks := make([]string, 0)
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Println("Essa é uma to-do list simples.")
-	fmt.Println("Comandos disponíveis: add, list, exit")
+	fmt.Print("Essa é uma to-do list simples.")
+	fmt.Print("Comandos disponíveis: add, list, exit")
 
 	for {
 		fmt.Println("\n> ")
@@ -26,6 +26,21 @@ func main() {
 			tasks = append(tasks, strings.TrimSpace(task))
 			fmt.Println("Tarefa adicionada:", task)
 
+		case "remove":
+			fmt.Print("Digite o número da tarefa que deseja remover: ")
+			var index int
+			_, err := fmt.Scanf("%d", &index)
+			if err != nil || index < 1 || index > len(tasks) {
+				fmt.Println("Número inválido.")
+				continue
+			}
+			tasks = append(tasks[:index-1], tasks[index:]...)
+			fmt.Println("Tarefa removida:", tasks[index-1])
+
+		case "clear":
+			tasks = make([]string, 0)
+			fmt.Println("Todas as tarefas foram removidas.")
+
 		case "list":
 			if len(tasks) == 0 {
 				fmt.Println("Nenhuma tarefa encontrada.")
@@ -35,6 +50,15 @@ func main() {
 					fmt.Printf("%d: %s\n", i+1, task)
 				}
 			}
+
+		case "help":
+			fmt.Println("Comandos disponíveis:")
+			fmt.Println("add - Adiciona uma nova tarefa")
+			fmt.Println("remove - Remove uma tarefa existente")
+			fmt.Println("clear - Remove todas as tarefas")
+			fmt.Println("list - Lista todas as tarefas")
+			fmt.Println("help - Mostra os comandos disponíveis")
+			fmt.Println("exit - Sai do programa")
 
 		case "exit":
 			fmt.Println("Até logo!")
