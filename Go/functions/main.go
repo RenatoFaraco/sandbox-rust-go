e8b00811ac53064e9d69763fd1233acb7c5cfd2e
RenatoFaraco <renato.faraco.f@gmail.com>
2025-05-10 23:14:30 -0300

    Aprendendo algumas funções e módulo em go

---
+++ Go/functions/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+var c, python, java bool
+
+func add(x, y int) int {
+	return x + y
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
+func main() {
+	fmt.Println("Imprime: ", add(1, 2))
+
+	a, b := swap("Teste", "Função")
+	fmt.Println("\n Imprime: ", a, b)
+
+	fmt.Println(split(17))
+
+	var i, J int = 1, 2
+	c, python, java := true, false, "no!"
+	fmt.Println("\n Imprime: ", i, J, c, python, java)
+
+	var x int
+	var y float64
+	var z bool
+	var w string
+	fmt.Printf("%v %v %v %q\n", x, y, z, w)
+
+	v := 42 + 52i // change me!
+	fmt.Printf("v is of type %T\n", v)
+	fmt.Println(v)
+}
