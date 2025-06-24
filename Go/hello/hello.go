c3732a8f42ec96e7f802881b15624850978171c4
RenatoFaraco <renato.faraco.f@gmail.com>
2025-06-23 23:29:22 -0300

    Just another commit in go

--- Go/hello/hello.go
+++ Go/hello/hello.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("🐹 Hello World em Go!")
+	fmt.Println("Jus another commit in Go!")
 
-	sum:= 2 + 2
+	sum := 2 + 2
 	fmt.Println("2 + 2 =", sum)
 }
