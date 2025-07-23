eab85f2ddbf4463fa4314a6e61965ed9456bfaee
RenatoFaraco <renato.faraco.f@gmail.com>
2025-07-22 23:50:56 -0300

    fibonacci em go

---
+++ Go/fibonacci/fibonacci.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func fibonacci(n int) []int {
+	if n <= 0 {
+		return []int{}
+	} else if n == 1 {
+		return []int{0}
+	}
+
+	seq := make([]int, n)
+	seq[0] = 0
+	seq[1] = 1
+
+	for i := 2; i < n; i++ {
+		seq[i] = seq[i-1] + seq[i-2]
+	}
+
+	return seq
+}
+
+func main() {
+	n := 10
+	fibSeq := fibonacci(n)
+	fmt.Print("Os números de Fibonacci são: \n")
+
+	for i, num := range fibSeq {
+		fmt.Printf("Fib(%d) = %d \n", i, num)
+	}
+}
