c2e24ca2ef3e62372a8c55f32ad2503122ba9b4c
RenatoFaraco <renato.faraco.f@gmail.com>
2025-08-14 19:23:14 -0300

    x

--- Go/fibonacci/fibonacci.go
+++ Go/fibonacci/fibonacci.go
@@ -23,7 +23,7 @@ func fibonacci(n int) []int {
 func main() {
 	n := 100
 	fibSeq := fibonacci(n)
-	fmt.Print("Os números de Fibonacci são: \n")
+	fmt.Print("Os números de Fibonacci: \n")
 
 	for i, num := range fibSeq {
 		fmt.Printf("Fib(%d) = %d \n", i, num)
